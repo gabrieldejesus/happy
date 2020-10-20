2d7119cc7e02dd6abe6ca5936b46d6fa35d988f8
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-10-20 07:43:24 -0300

    upload episodeo 4

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// importar dependencia
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
+
+// iniciando o express
+const server = express();
+
+server
+// utilizando os arquivos estáticos
+.use(express.static('public'))
+
+// configurar template engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+// rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+// ligar o servidor
+server.listen(5000);
